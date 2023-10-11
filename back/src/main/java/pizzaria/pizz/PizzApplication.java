d63dbd000d7df9fb4cefead9eb22c04f8b2671d9
Gabriel Souza <2211600378@fema.edu.br>
2023-10-11 11:09:25 -0300

    adicionado projeto back-end

--- back/src/main/java/pizzaria/pizz/PizzApplication.java
+++ back/src/main/java/pizzaria/pizz/PizzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PizzApplication.class, args);
 	}
