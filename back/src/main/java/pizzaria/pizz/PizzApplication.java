15def397df3d411e5d230dd52e57c7249d89b10e
Gabriel Souza <2211600378@fema.edu.br>
2023-10-11 11:06:13 -0300

    criado projeto back-end

---
+++ back/src/main/java/pizzaria/pizz/PizzApplication.java
@@ -0,0 +1,13 @@
+package pizzaria.pizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/pizzaria/pizz/PizzApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzaria.pizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
